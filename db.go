fe8e23fd7b579e72619c283e267cfad7b8913369
Beloved-vince <vinceoludare@gmail.com>
2023-07-22 11:57:45 +0100

    initialize module

---
+++ db.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/jinzhu/gorm"
+
+var db *gorm.DB
+
+func ConnectDatabase() {
+	connectionString := "host=localhost port=5432 user=postgres dnname=postgres password=Oludare2001 sslmode=disable"
+	var err error
+
+	db, err = gorm.Open("postgres", connectionString)
+
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate()
+}
