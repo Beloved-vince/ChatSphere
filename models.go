958b58d100266d8850e1eb737f1b8a2e396efd91
Beloved-vince <vinceoludare@gmail.com>
2023-07-22 12:27:03 +0100

    models declaration

--- models.go
+++ models.go
@@ -2,5 +2,13 @@ package main
 
 import "github.com/jinzhu/gorm"
 
-
-type 
+type User struct {
+	gorm.Model
+	ID        uint
+	FirstName string
+	LastName  string
+	Username  string
+	Email     *string
+	CreatedAt string
+	UpdatedAt string
+}
