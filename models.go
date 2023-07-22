d96bcf01fc1d6d93573c6e8b7d07cac04ed43fcb
Beloved-vince <vinceoludare@gmail.com>
2023-07-22 12:50:12 +0100

    models declaration

--- models.go
+++ models.go
@@ -1,14 +1,18 @@
 package main
 
 import "github.com/jinzhu/gorm"
+import "time"
 
 type User struct {
 	gorm.Model
-	ID        uint
-	FirstName string
-	LastName  string
-	Username  string
-	Email     *string
-	CreatedAt string
-	UpdatedAt string
+	ID        uint 			`gorm:"primary_key;auto_increment"`
+	FirstName string 		`gorm:"not null"`
+	LastName  string 		`gorm:"not null"`
+	Username  string 		`gorm:"not null;unique"`
+	Email     *string 		`gorm:"not null;unique"`
+	CreatedAt time.Time  	`gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time 	`gorm:"default:CURRENT_TIMESTAMP"`
 }
+
+
+type
